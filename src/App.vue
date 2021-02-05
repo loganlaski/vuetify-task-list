a5613629d02d3ab1f2ba07571d0ebd8f5633227b
Logan Laski <logan.laski@gmail.com>
2021-02-05 09:41:54 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app id="app">
+    <v-content>
+      <v-container id="container">
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<style lang="stylus" scope>
+#app
+  background: #eeeeee
+
+#container
+  max-width: 550px
+  padding: 8px
+</style>
---
+++ src/components/FooterInfo.vue
@@ -0,0 +1,21 @@
+<template>
+<footer class="caption">
+  <p>Double-click to edit tasks</p>
+  <p><a href="https://github.com/loganlaski" target="_blank" rel="noopener">View on GitHub</a></p>
+</footer>
+</template>
+
+<style lang="stylus">
+footer
+  margin: 65px auto 0
+  color: #bbbbbb
+  text-shadow: 0 1px 0 rgba(255, 255, 255, 0.5)
+  p
+    line-height: 1
+    font-size: 16px
+  a
+    color: inherit
+    text-decoration: none
+    &:hover
+      text-decoration: underline
+</style>
